a9c7fb8ebba157da51a4d7080a8fd3e3d776477e
niculas1989 <nicolas.maranzano@libero.it>
2022-01-20 14:42:28 +0100

    add comments + array (bikes)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 console.log('JS OK');
 
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
+*/
+
+//! FIRST STEP
+//| creazione della lista (array) di oggetti
+
+const bikes = [
+    { nome: 'ciccio', peso: 3 },
+    { nome: 'bianchi', peso: 5 },
+    { nome: 'pasticcio', peso: 1 },
+    { nome: 'chicco', peso: 8 },
+    { nome: 'pasticcio', peso: 3 },
+    { nome: 'peppa', peso: 7 },
+    { nome: 'crispy', peso: 3 }
+]
+
+//! SECOND STEP
+//| ciclo in cui cercare ciò che mi interessa
+
+
