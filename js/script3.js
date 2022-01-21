25c607078e4c2988980b1b6dfa6fbe4008c0b7ae
niculas1989 <nicolas.maranzano@libero.it>
2022-01-21 14:07:20 +0100

    added two more scripts - today's exercises

---
+++ js/script3.js
@@ -0,0 +1,6 @@
+console.log('JS OK');
+/*
+Scrivere una funzione che accetti tre argomenti, un array e due numeri(a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+Usiamo i nuovi metodi degli array visti oggi in classe.
+*/
