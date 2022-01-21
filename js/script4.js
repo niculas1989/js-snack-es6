64150ab62a559975fff2aa0ae0dfacac12fbf8e4
niculas1989 <nicolas.maranzano@libero.it>
2022-01-21 14:59:50 +0100

    added new array

--- js/script4.js
+++ js/script4.js
@@ -9,3 +9,34 @@ ES (marco de iulio => Marco de iulio);
 BONUS  nei nomi sulle targhe l'iniziale di ogni parola deve essere maiuscola
 ES.: (marco de iulio  => Marco De Iulio)
 */
+
+//# step1 - creazione lista studenti
+
+const students = [
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'salvatore pitanza', id: 40, voto: 40 },
+    { nome: 'adriana santorio', id: 500, voto: 90 },
+    { nome: 'laura bigoni', id: 10, voto: 80 },
+    { nome: 'nicolas maranzano', id: 350, voto: 90 },
+    { nome: 'matteo imbimbo', id: 10, voto: 70 },
+    { nome: 'marco trucellito', id: 90, voto: 85 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+    { nome: 'marco de iulio', id: 210, voto: 70 },
+]
